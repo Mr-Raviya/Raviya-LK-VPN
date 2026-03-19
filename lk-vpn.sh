787f2a33c1d13f4769b93fafc76d70b6bf954677
Ravindu Karunarathne <136822205+Mr-Raviya@users.noreply.github.com>
2026-03-19 10:47:40 +0530

    Add lk-vpn.sh script for VPN installation

---
+++ lk-vpn.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo wget -4 -q -O /usr/local/bin/raviya-lk-vpn https://raw.githubusercontent.com/Mr-Raviya/Raviya-LK-VPN/refs/heads/main/lk-vpn
+sudo chmod +x /usr/local/bin/raviya-lk-vpn
+lk-vpn
