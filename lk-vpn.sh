db99d293e19597c73b4db6cb678157c5971646aa
Ravindu Karunarathne <136822205+Mr-Raviya@users.noreply.github.com>
2026-03-19 11:12:34 +0530

    Rename lk-vpn script to raviya-lk-vpn

--- lk-vpn.sh
+++ lk-vpn.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # 1. පරණ ෆයිල් තිබේ නම් ඒවා ඉවත් කිරීම
-sudo rm -f /usr/local/bin/lk-vpn
+sudo rm -f /usr/local/bin/raviya-lk-vpn
 sudo rm -f /usr/local/bin/lk.conf
 
 # 2. නිවැරදි නමින් ලින්ක් එකෙන් බාගත කිරීම
-sudo wget -4 -q -O /usr/local/bin/lk-vpn https://raw.githubusercontent.com/Mr-Raviya/Raviya-LK-VPN/main/lk-vpn
+sudo wget -4 -q -O /usr/local/bin/raviya-lk-vpn https://raw.githubusercontent.com/Mr-Raviya/Raviya-LK-VPN/main/raviya-lk-vpn
 sudo wget -4 -q -O /usr/local/bin/lk.conf https://raw.githubusercontent.com/Mr-Raviya/Raviya-LK-VPN/main/lk.conf
 
 # 3. අවසර ලබා දීම
-sudo chmod +x /usr/local/bin/lk-vpn
+sudo chmod +x /usr/local/bin/raviya-lk-vpn
 
 # 4. ස්ක්‍රිප්ට් එක ක්‍රියාත්මක කිරීම
-/usr/local/bin/lk-vpn
+/usr/local/bin/raviya-lk-vpn
