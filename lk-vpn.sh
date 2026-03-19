5eccaeefd875a8e8d2769095ecdab6a82de7adfc
Ravindu Karunarathne <136822205+Mr-Raviya@users.noreply.github.com>
2026-03-19 11:09:03 +0530

    Update VPN script to download new lk-vpn and config

--- lk-vpn.sh
+++ lk-vpn.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-sudo wget -4 -q -O /usr/local/bin/raviya-lk-vpn https://raw.githubusercontent.com/Mr-Raviya/Raviya-LK-VPN/main/raviya-lk-vpn
-sudo chmod +x /usr/local/bin/raviya-lk-vpn
-raviya-lk-vpn
+# ප්‍රධාන ස්ක්‍රිප්ට් එක බාගත කිරීම
+sudo wget -4 -q -O /usr/local/bin/lk-vpn https://raw.githubusercontent.com/Mr-Raviya/Raviya-LK-VPN/main/lk-vpn
+
+# Config ෆයිල් එක බාගත කිරීම (මෙය අත්‍යවශ්‍යයි)
+sudo wget -4 -q -O /usr/local/bin/lk.conf https://raw.githubusercontent.com/Mr-Raviya/Raviya-LK-VPN/main/lk.conf
+
+# Execute permission ලබා දීම
+sudo chmod +x /usr/local/bin/lk-vpn
+
+# ස්ක්‍රිප්ට් එක රන් කිරීම
+lk-vpn
