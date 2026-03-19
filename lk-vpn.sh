56eb5e2d4e0d6ef3333b2f8f246efca95605a0ab
Ravindu Karunarathne <136822205+Mr-Raviya@users.noreply.github.com>
2026-03-19 11:10:47 +0530

    Refactor lk-vpn.sh to improve file handling

--- lk-vpn.sh
+++ lk-vpn.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-# ප්‍රධාන ස්ක්‍රිප්ට් එක බාගත කිරීම
-sudo wget -4 -q -O /usr/local/bin/lk-vpn https://raw.githubusercontent.com/Mr-Raviya/Raviya-LK-VPN/main/lk-vpn
 
-# Config ෆයිල් එක බාගත කිරීම (මෙය අත්‍යවශ්‍යයි)
+# 1. පරණ ෆයිල් තිබේ නම් ඒවා ඉවත් කිරීම
+sudo rm -f /usr/local/bin/lk-vpn
+sudo rm -f /usr/local/bin/lk.conf
+
+# 2. නිවැරදි නමින් ලින්ක් එකෙන් බාගත කිරීම
+sudo wget -4 -q -O /usr/local/bin/lk-vpn https://raw.githubusercontent.com/Mr-Raviya/Raviya-LK-VPN/main/lk-vpn
 sudo wget -4 -q -O /usr/local/bin/lk.conf https://raw.githubusercontent.com/Mr-Raviya/Raviya-LK-VPN/main/lk.conf
 
-# Execute permission ලබා දීම
+# 3. අවසර ලබා දීම
 sudo chmod +x /usr/local/bin/lk-vpn
 
-# ස්ක්‍රිප්ට් එක රන් කිරීම
-lk-vpn
+# 4. ස්ක්‍රිප්ට් එක ක්‍රියාත්මක කිරීම
+/usr/local/bin/lk-vpn
